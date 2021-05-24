9e3526d1816935239852664f4290ba408eb7901b
Júnior Nunes <denisonprobable13@gmail.com>
2021-05-24 20:09:04 -0300

    Estilização parcial da página index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
 import { firestore } from '../utils/firebase';
 
@@ -32,21 +33,89 @@ export default function Home() {
   }
 
   return (
-    <div className="App">
-      <div>
-        <h1>Visualizar Cadastros</h1>
+    <Container>
+      <div className="App">
+        <div>
+          <h1>Visualizar Cadastros</h1>
+        </div>
+        <div className="tableContainer">
+          <table>
+            <tr className="tableHeader">
+              <td>NOME</td>
+              <td>IDADE</td>
+              <td>ESTADO CIVIL</td>
+              <td>CPF</td>
+              <td>CIDADE</td>
+              <td>ESTADO</td>
+            </tr>
+            {
+              registers.map(register => {
+                return(
+                  <tr key={register.id}>
+                    <td className="name">{register.Name}</td>
+                    <td>{register.Age}</td>
+                    <td>{register.MaritalStatus}</td>
+                    <td>{register.CPF}</td>
+                    <td>{register.City}</td>
+                    <td>{register.State}</td>
+                    <td><a href="#">Editar</a></td>
+                  </tr>
+                );
+              })
+            }
+            <tr className="tableFooter">
+              <td>Mostrando 1 até 6 de 20 resultados</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td><button>Anterior</button></td>
+              <td><button>Próximo</button></td>
+            </tr>
+          </table>
+        </div>
       </div>
-      <div>
-        {
-          registers.map(register => {
-            return(
-              <div key={register.id}>
-                {register.Name}
-              </div>
-            );
-          })
-        }
-      </div>
-    </div>
+    </Container>
   )
 }
+
+const Container = styled.section`
+  .tableContainer{
+    display: flexbox;
+    align-items: center;
+    justify-content: center;
+    height: 80vh;
+
+    table{
+      align-items: flex-start;
+      padding: .5rem;
+      border: 1px solid #E8E8E8;
+      border-bottom: 2px solid #D7D7D7;
+      border-radius: 10px;
+
+      .tableHeader{
+        font-size: .75rem;
+        text-align: start;
+        td{
+          margin-top: 0;
+          padding-bottom: 10px;
+        }
+      }
+      
+      tr{
+        td{
+          color: #A3A3A3;
+          padding: 10px;
+        }
+      }
+
+      .name{
+        color: black
+      }
+
+      .tableFooter{
+        border: 1px solid black
+      }
+    }
+  }
+`;
