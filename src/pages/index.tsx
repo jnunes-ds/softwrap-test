93f14b0252e20e727adb63fd58d4af2005248bfa
Júnior Nunes <denisonprobable13@gmail.com>
2021-05-23 23:16:37 -0300

    env

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,4 @@
-import { useEffect } from 'react';
-import { getRecords } from '../utils/Table';
-
-
 export default function Home() {
-
   return (
     <div className="App">
       <h1>Visualizar Cadastros</h1>
--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -2,13 +2,13 @@ import firebase from "firebase";
 import "firebase/firestore";
 
 const config = {
-    apiKey: "AIzaSyBQ3jDJ_jT1cYFhtWgR7n11Cqs_ptSOrys",
-    authDomain: "softwrap-test-3b16d.firebaseapp.com",
-    projectId: "softwrap-test-3b16d",
-    storageBucket: "softwrap-test-3b16d.appspot.com",
-    messagingSenderId: "714109861651",
-    appId: "1:714109861651:web:1cb249ae34d8973341e4e6",
-    measurementId: "G-PB9RYH5GCZ"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 if(!firebase.apps.length){
