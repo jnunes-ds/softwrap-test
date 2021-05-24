52fdffdeb5ef9a1378c42950e308b36b41f4a2a8
Júnior Nunes <denisonprobable13@gmail.com>
2021-05-24 19:28:46 -0300

    Renderizando informações do firestore no index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,52 @@
+import { useEffect, useState } from 'react';
+
+import { firestore } from '../utils/firebase';
+
+
+
 export default function Home() {
+  const [registers, setRegisters] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const ref = firestore.collection("tabela");
+
+  function getRegisters(){
+    setLoading(true);
+    ref.onSnapshot((querySnapshot) => {
+      const items = [];
+      querySnapshot.forEach((doc) => {
+        items.push(doc.data());
+      });
+      setRegisters(items);
+      console.log(registers)
+      setLoading(false);
+    });
+  }
+
+  useEffect(() => {
+    getRegisters();
+  }, []);
+
+  if(loading){
+    return <h1>Loading...</h1>
+  }
+
   return (
     <div className="App">
-      <h1>Visualizar Cadastros</h1>
+      <div>
+        <h1>Visualizar Cadastros</h1>
+      </div>
+      <div>
+        {
+          registers.map(register => {
+            return(
+              <div key={register.id}>
+                {register.Name}
+              </div>
+            );
+          })
+        }
+      </div>
     </div>
   )
 }
