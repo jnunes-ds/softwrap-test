a2e073e1055b88584847abbcda40dc1559259e04
Júnior Nunes <denisonprobable13@gmail.com>
2021-05-25 00:41:40 -0300

    Botões agora podem ser estilizados durante a implementaçaõ do componente.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,48 @@
 import styled from "styled-components";
 
+interface ButtonColorsProps{
+    bgNormal: string;
+    borderNormal: string;
+    textColorNormal: string;
+    bgHover: string;
+    borderHover: string;
+    textColorHover: string;
+}
 interface ButtonProps{
     name: string;
     onClick?: VoidFunction;
+    color?: ButtonColorsProps;
 }
 
-export default function Button({name, onClick}: ButtonProps){
+const ButtonColor: ButtonColorsProps = {
+    bgNormal: '#FFF',
+    borderNormal: '1px solid black',
+    textColorNormal: 'black',
+    bgHover: 'blue',
+    borderHover: 'transparent',
+    textColorHover: '#FFF'
+}
+
+
+export default function Button({name, onClick, color=ButtonColor}: ButtonProps){
+    const StyledButton = styled.button`
+    border: ${color.borderNormal};
+    border-radius: 10px;
+    color: ${color.textColorNormal};
+    background-color:${color.bgNormal};
+    padding: .75rem;
+    margin: .5rem;
+    font-size: 15px;
+
+    :hover{
+        background-color: ${color.bgHover};
+        color: ${color.textColorHover};
+        border: ${color.borderHover};
+        cursor: pointer;
+    }
+    `;
+
+
     return (
         <div>
             <StyledButton onClick={onClick}>
@@ -15,18 +52,3 @@ export default function Button({name, onClick}: ButtonProps){
     );
 }
 
-const StyledButton = styled.button`
-    border: 1px solid #E8E8E8;
-    border-radius: 10px;
-    background-color: #FFF;
-    padding: .75rem;
-    margin: .5rem;
-    font-size: 15px;
-
-    :hover{
-        background-color: blue;
-        color: #FFF;
-        border: transparent;
-        cursor: pointer;
-    }
-`;
