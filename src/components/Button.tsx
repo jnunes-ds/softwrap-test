95aa3baf6685a0c288230c2162118df7ce4dbd60
Júnior Nunes <denisonprobable13@gmail.com>
2021-05-25 13:03:20 -0300

    Estilização condicional do componente botão

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 
 interface ButtonColorsProps{
-    bgNormal: string;
-    borderNormal: string;
-    textColorNormal: string;
-    bgHover: string;
-    borderHover: string;
-    textColorHover: string;
+    bgNormal?: string;
+    borderNormal?: string;
+    textColorNormal?: string;
+    bgHover?: string;
+    borderHover?: string;
+    textColorHover?: string;
 }
 interface ButtonProps{
     name: string;
@@ -26,18 +26,18 @@ const ButtonColor: ButtonColorsProps = {
 
 export default function Button({name, onClick, color=ButtonColor}: ButtonProps){
     const StyledButton = styled.button`
-    border: ${color.borderNormal};
+    border: ${color.borderNormal || '1px solid black'};
     border-radius: 10px;
-    color: ${color.textColorNormal};
-    background-color:${color.bgNormal};
+    color: ${color.textColorNormal || 'black'};
+    background-color:${color.bgNormal || '#FFF'};
     padding: .75rem;
     margin: .5rem;
     font-size: 15px;
 
     :hover{
-        background-color: ${color.bgHover};
-        color: ${color.textColorHover};
-        border: ${color.borderHover};
+        background-color: ${color.bgHover || 'blue'};
+        color: ${color.textColorHover || '#FFF'};
+        border: ${color.borderHover || 'transparent'};
         cursor: pointer;
     }
     `;
